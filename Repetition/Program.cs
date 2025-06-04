ea9ec5e6ab1490198c0a2f46ca585fb4308cd339
LucaoCorrea <lucascorrea1505@gmail.com>
2025-06-04 14:36:17 -0300

    for

---
+++ Repetition/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Channels;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int sum = 0;
+
+        for (int i = 1; i <= 10; i++)
+        {
+            sum += i;
+        }
+        Console.WriteLine("A soma dos números de 1 a 10 é: " + sum);
+        Console.WriteLine("Pressione qualquer tecla para sair...");
+        Console.ReadKey();
+
+    }
+}
